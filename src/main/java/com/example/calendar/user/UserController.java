2ed63b1375125ca5564cd1a898ddeb5d7091e5ca
JeremySun0731 <edwardsun200407@gmail.com>
2026-02-23 10:38:53 -0500

    fix the package!

--- src/main/java/com/example/calendar/user/UserController.java
+++ src/main/java/com/example/calendar/user/UserController.java
@@ -1,15 +1,23 @@
 package com.example.calendar.user;
 
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private UserService userService;
+
+    private final UserService userService;
+
     public UserController(UserService userService){
         this.userService = userService;
     }
-    @PostMapping("/register")
-    public User register(@RequestBody User user) {
+
+    @GetMapping("/register")
+    public User register() {
+        User user = new User();
+        user.setUserName("Test");
+        user.setEmail("test@test.com");
+        user.setPassword("123456");
         return userService.register(user);
     }
 }
